c1bcd15241c847910f465f74b4ad55fef655bd7c
Aaryan-J <aaryanjoshi217@gmail.com>
2026-09-26 00:59:39 +0530

    sticky notes now display the mainframe task codes on them when interacted and added some details to the environment

--- Scripts/Tasks/interactable.gd
+++ Scripts/Tasks/interactable.gd
@@ -12,12 +12,20 @@ signal taskFixed(taskNode)
 func interact(playerNode: CharacterBody3D):
 	if taskName == "TerminalCodeNote":
 		var manager = get_tree().get_first_node_in_group("gameManagerGroup")
+		var displayLabel = get_node_or_null("CodeDisplay")
+
 		if manager and manager.activeTerminalCode[noteIndex] != "":
-			print("STICKY NOTE ", noteIndex + 1, ": Reading sequence line -> ", manager.activeTerminalCode[noteIndex]) # put UI print label thingy here
+			var activeCode = manager.activeTerminalCode[noteIndex]
+			print("STICKY NOTE ", noteIndex + 1, ": Reading sequence line -> ", activeCode) # put UI print label thingy here
+			if displayLabel:
+				displayLabel.text = activeCode
+				displayLabel.modulate = Color.DARK_RED
 		else:
 			print("STICKY NOTE BLANK")
+			if displayLabel:
+				displayLabel.text = "...."
 		return
-	
+
 	if isBroken:
 		startMinigame(playerNode)
 	else:
@@ -25,18 +33,18 @@ func interact(playerNode: CharacterBody3D):
 
 func startMinigame(playerNode: CharacterBody3D):  # player node for when imma add minigames
 	var minigameWindow = playerNode.get_node_or_null("HUD/MinigameWindow")
-	
+
 	if minigameWindow:
 		minigameWindow.openMinigame(self)
 	else:
 		print("HUD minigame window not found. Auto complete task")
 		completeTask()
-	
+
 func completeTask():
 	isBroken = false
 	taskFixed.emit(self)
 	print(taskName, " has been repaired.")
-	
+
 func breakTask():
 	isBroken = true
 	taskBroken.emit(self)
